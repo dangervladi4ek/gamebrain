fd560db24bf67c8552d8b02369d3adf33e185519
dangervladi4ek <peska4@bk.ru>
2024-05-08 15:54:56 +0300

    TZ

---
+++ bin/brain-games.js
@@ -0,0 +1,5 @@
+import { question } from 'readline-sync';
+
+console.log('Welcome to the Brain Games!');
+console.log('Hi ' + question('May I have your name? ') + '!');
+console.log(`Hello, ${playerName}! Welcome to the game!`);
